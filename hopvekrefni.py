8ff5a2aafcc97b49f5bff56d6e955dd6e6075939
CupOfCake <82550028+CupOfCake@users.noreply.github.com>
2023-08-22 11:53:19 +0000

    Update hopvekrefni.py

--- hopvekrefni.py
+++ hopvekrefni.py
@@ -1,11 +1,9 @@
 def add_numbers(a, b, c):
-    print(a + b + c)
+    print(f"The sum of these numbers is: {a + b + c}")
     print("halló stærðfræði")
 
-a = int(input("enter a: "))
-b = int(input("enter b: "))
-c = int(input("enter c: "))
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter a number: "))
+third_number = int(input("Enter a number: "))
 
-add_numbers(a, b, c)
-
-# :)  
+add_numbers(first_number, second_number, third_number)
