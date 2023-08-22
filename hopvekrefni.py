5d6cfe169be42497e0141be67f832cfe39008d08
CupOfCake <82550028+CupOfCake@users.noreply.github.com>
2023-08-22 11:20:45 +0000

    verkefni

---
+++ hopvekrefni.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b, c):
+    print(a + b + c)
+    print("halló stærðfræði")
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+c = int(input("enter c: "))
+
+add_numbers(a, b, c)
+
+# :)  
